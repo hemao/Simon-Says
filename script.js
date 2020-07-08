e43876373b163fce49bd22603969218970cf28f7
HemaO <hema.omprakash@gmail.com>
2020-07-07 20:23:37 -0400

    added layout in index.html, updated style.css

---
+++ script.js
@@ -0,0 +1,15 @@
+//define the colors array
+let colors = ["red", "green", "blue","yellow","orange", "pink"]
+/*
+//grab the board to add colored boxes
+let board = document.querySelector(".board")
+
+// add boxes(divs) to hold each color
+for(let i=0; i<colors.length; i++){
+    let box = document.createElement("div");
+    box.innerText = "aaaaaaaaa"
+    box.classList.add("box")
+    //append the box to the board
+    board.appendChild(box);
+}
+*/
