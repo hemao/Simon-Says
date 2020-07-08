94cfbb9d910a53edbb0d639a6e9d3ac7e55748fc
HemaO <hema.omprakash@gmail.com>
2020-07-08 10:14:46 -0400

    aligned button in grid 1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let colors = ["red", "green", "blue","yellow","orange", "pink"]
 
 //grab the board to add colored boxes
 let main = document.querySelector("main")
-
+/*
 // add boxes(divs) to hold each color
 for(let i=0; i<colors.length; i++){
     let box = document.createElement("div");
@@ -11,3 +11,4 @@ for(let i=0; i<colors.length; i++){
     //append the box to the board
     main.appendChild(box);
 }
+*/
