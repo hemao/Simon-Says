436065f1927693f5ecf2c9676ddd0439e077fe59
HemaO <hema.omprakash@gmail.com>
2020-07-08 07:36:00 -0400

    removed board tag, modified for loop

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //define the colors array
 let colors = ["red", "green", "blue","yellow","orange", "pink"]
-/*
+
 //grab the board to add colored boxes
-let board = document.querySelector(".board")
+let main = document.querySelector("main")
 
 // add boxes(divs) to hold each color
 for(let i=0; i<colors.length; i++){
     let box = document.createElement("div");
-    box.innerText = "aaaaaaaaa"
-    box.classList.add("box")
+    box.classList.add(colors[i])
     //append the box to the board
-    board.appendChild(box);
+    main.appendChild(box);
 }
-*/
