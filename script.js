2de36ed20ee230b1c08e694d8fb4fcd3b81c6616
HemaO <hema.omprakash@gmail.com>
2020-07-08 11:05:59 -0400

    removed all button divs and added logic to handle that in scripts.js

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ let colors = ["red", "green", "blue","yellow","orange", "pink"]
 
 //grab the board to add colored boxes
 let main = document.querySelector("main")
-/*
+
 // add boxes(divs) to hold each color
 for(let i=0; i<colors.length; i++){
-    let box = document.createElement("div");
+    let box = document.createElement("div")
     box.classList.add(colors[i])
+    //add button inside box
+    let button = document.createElement("button")
+    //add event listener to button
+    button.addEventListener("click",highlightBtn)
+    button.style.backgroundColor = colors[i]
+    //append button to box
+    box.appendChild(button)
     //append the box to the board
-    main.appendChild(box);
+    main.appendChild(box)
 }
-*/
+
+//highlights the button when clicked
+function highlightBtn(){
+
+}
